0db1d485bd0b4110b739cd5f4efacf433c31cfef
Tom Wright <tom@inflatablecookie.com>
2012-09-25 17:09:38 +0100

    Added stub for devtools icon manager
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/devtools/theme/_menus/Index_WebCore.php
+++ directory/devtools/theme/_menus/Index_WebCore.php
@@ -13,14 +13,12 @@ use df\arch;
 class Index_WebCore extends arch\navigation\menu\Base {
 
     protected function _createEntries(arch\navigation\IEntryList $entryList) {
-    	/*
         $entryList->addEntries(
-            $entryList->newLink('~devtools/theme/layouts/', 'Layout setup')
-                ->setId('layouts')
-    			->setDescription('Configure layout definitions for dynamic view generation')
-    			->setIcon('layout')
-    			->setWeight(10)
+            $entryList->newLink('~devtools/theme/icons/', 'Icon control')
+                ->setId('icons')
+    			->setDescription('Generate icon fonts and an icon map for your theme')
+    			->setIcon('icon')
+    			->setWeight(20)
 		);
-		*/
     }
 }
---
+++ directory/devtools/theme/icons/HttpController.php
@@ -0,0 +1,23 @@
+<?php 
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\directory\devtools\theme\icons;
+
+use df;
+use df\core;
+use df\apex;
+use df\arch;
+    
+class HttpController extends arch\Controller {
+
+    const DEFAULT_ACCESS = arch\IAccess::DEV;
+
+    public function indexHtmlAction() {
+    	$container = $this->aura->getWidgetContainer();
+    	$container->push('Coming soon...');
+
+    	return $container;
+    }
+}
