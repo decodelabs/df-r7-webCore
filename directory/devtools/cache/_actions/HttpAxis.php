fcc5234b484e87ae27776dc191fbe85cf6f44661
Tom Wright <tom@inflatablecookie.com>
2013-01-16 13:19:07 +0000

    Updated reference to MetaData
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/devtools/cache/_actions/HttpAxis.php
+++ directory/devtools/cache/_actions/HttpAxis.php
@@ -24,7 +24,7 @@ class HttpAxis extends arch\form\Action {
     protected function _createUi() {
         $form = $this->content->addForm();
         $keys = $this->_cache->getKeys();
-        $info = axis\Unit::getUnitMetadata($keys);
+        $info = axis\Unit::getUnitMetaData($keys);
 
         $form->push(
             $this->html->collectionList($info)
