155632c1513f98ce3f294a5f0767f829deae2d66
Tom Wright <tom@inflatablecookie.com>
2013-01-27 14:58:02 +0000

    Removed assets from theme router path
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/front/theme/HttpRouter.php
+++ directory/front/theme/HttpRouter.php
@@ -10,25 +10,25 @@ class HttpRouter implements arch\IRouter {
     public function routeIn(arch\IRequest $request) {
         $path = $request->getPath();
         $path->shift();
-        
+
         $query = $request->getQuery();
         $query->theme = $path->shift();
         
-        $request->setPath('theme/'.$path->shift());
+        $request->setPath('theme/assets');
         $query->file = (string)$path;
-        
+
         return $request;
     }
     
     public function routeOut(arch\IRequest $request) {
         $path = $request->getPath();
         $query = $request->getQuery();
-        
+
         if(!isset($query['theme']) || !isset($query['file'])) {
             return false;
         }
         
-        $path->put(1, $query->get('theme'));
+        $path->set(-1, $query->get('theme'));
         $path->push($query->get('file'));
         $query->remove('theme')->remove('file');
         
