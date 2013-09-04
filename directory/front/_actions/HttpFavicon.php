183ad954aa891da51bc8a5e2856170744801b84e
Tom Wright <tom@inflatablecookie.com>
2013-09-04 18:43:10 +0100

    Added default favicon
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ directory/front/_actions/HttpFavicon.php
@@ -0,0 +1,21 @@
+<?php 
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\directory\front\_actions;
+
+use df;
+use df\core;
+use df\apex;
+use df\arch;
+    
+class HttpFavicon extends arch\Action {
+
+    const CHECK_ACCESS = false;
+    const DEFAULT_ACCESS = arch\IAccess::ALL;
+
+    public function executeAsIco() {
+        return $this->http->fileResponse($this->findFile('apex/themes/shared/assets/favicon.ico'));
+    }
+}
