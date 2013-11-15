f2aedd2a690df3014a5fa52bb91b4ad2138a1c79
Tom Wright <tom@inflatablecookie.com>
2013-11-15 16:47:06 +0000

    Added shortcuts to invite senders in record
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- models/user/invite/Record.php
+++ models/user/invite/Record.php
@@ -12,5 +12,15 @@ use df\opal;
 
 class Record extends opal\record\Base {
     
-    
+    public function send($templatePath=null, $templateLocation=null) {
+        return $this->getRecordAdapter()->send($this, $templatePath, $templateLocation);
+    }
+
+    public function resend($templatePath=null, $templateLocation=null) {
+        return $this->getRecordAdapter()->resend($this, $templatePath, $templateLocation);
+    }
+
+    public function claim(user\IClientDataObject $client) {
+        return $this->getRecordAdapter()->claim($this, $client);
+    }
 }
