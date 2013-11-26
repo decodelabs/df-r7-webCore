191f841557a158b5d5df25cdd2e515b1cc711129
Tom Wright <tom@inflatablecookie.com>
2013-11-26 19:18:50 +0000

    Added basic cookie notice element and action
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ directory/front/_actions/HttpDismissCookieNotice.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\directory\front\_actions;
+
+use df;
+use df\core;
+use df\apex;
+use df\arch;
+use df\aura;
+
+class HttpDismissCookieNotice extends arch\Action {
+
+    const CHECK_ACCESS = false;
+    const DEFAULT_ACCESS = arch\IAccess::ALL;
+
+    public function execute() {
+        $theme = aura\theme\Base::factory($this->context);
+        $this->http->setCookie($theme::COOKIE_NOTICE_COOKIE, 1);
+
+        return $this->http->defaultRedirect();
+    }
+}
