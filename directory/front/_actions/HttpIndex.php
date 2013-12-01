33444c207e5025e6f3ab6f3146f755fa558fcc60
Tom Wright <tom@inflatablecookie.com>
2013-12-01 16:43:11 +0000

    Updated root index action
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/front/_actions/HttpIndex.php
+++ directory/front/_actions/HttpIndex.php
@@ -3,13 +3,13 @@
  * This file is part of the Decode Framework
  * @license http://opensource.org/licenses/MIT
  */
-namespace df\apex\directory\front;
+namespace df\apex\directory\front\_actions;
 
 use df;
 use df\core;
 use df\arch;
 
-class HttpController extends arch\Controller {
+class HttpIndex extends arch\Action {
     
     const CHECK_ACCESS = false;
     const DEFAULT_ACCESS = arch\IAccess::ALL;
