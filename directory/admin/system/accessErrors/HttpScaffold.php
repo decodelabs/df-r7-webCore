8255f8085c6cdc22f7cf7d3fe75a30250f28acbc
Tom Wright <tom@inflatablecookie.com>
2014-05-22 15:07:13 +0100

    Converted access errors admin to use scaffold
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ directory/admin/system/accessErrors/HttpScaffold.php
@@ -0,0 +1,189 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\directory\admin\system\accessErrors;
+
+use df;
+use df\core;
+use df\apex;
+use df\arch;
+use df\opal;
+
+class HttpScaffold extends arch\scaffold\template\RecordAdmin {
+    
+    const DIRECTORY_TITLE = 'Access error logs';
+    const DIRECTORY_ICON = 'lock';
+    const RECORD_ADAPTER = 'axis://log/AccessError';
+    const RECORD_NAME_KEY = 'date';
+    const RECORD_KEY_NAME = 'error';
+
+    const CAN_ADD_RECORD = false;
+    const CAN_EDIT_RECORD = false;
+
+    protected $_recordListFields = [
+        'date' => true,
+        'mode' => true,
+        'code' => true,
+        'request' => true,
+        'message' => true,
+        'user' => true,
+        'isProduction' => true,
+        'actions' => true
+    ];
+
+    protected $_recordDetailsFields = [
+        'date' => true,
+        'user' => true,
+        'mode' => true,
+        'code' => true,
+        'request' => true,
+        'query' => true,
+        'userAgent' => true,
+        'message' => true
+    ];
+
+
+// Record data
+    protected function _prepareRecordListQuery(opal\query\ISelectQuery $query, $mode) {
+        $query->importRelationBlock('user', 'link');
+    }
+
+    protected function _describeRecord($record) {
+        return $record['mode'].' '.$record['code'].' - '.$this->format->date($record['date']);
+    }
+
+
+// Components
+    public function addIndexHeaderBarOperativeLinks($menu, $bar) {
+        $menu->addLinks(
+            $this->html->link(
+                    $this->uri->request('~admin/system/access-errors/delete-all', true),
+                    $this->_('Delete all errors')
+                )
+                ->setIcon('delete')
+        );
+    }
+
+
+// Fields
+    public function describeModeField($list, $mode) {
+        $list->addField('mode', function($error) use($mode) {
+            $output = $error['mode'];
+
+            if($mode != 'list') {
+                $output = [
+                    $output, ' ',
+                    $this->html->element('sup', '('.($error['isProduction'] ? $this->_('production') : $this->_('testing')).')')
+                        ->addClass($error['isProduction'] ? 'state-error' : 'state-warning')
+                ];
+            }
+
+            return $output;
+        });
+    }
+
+    public function describeCodeField($list, $mode) {
+        $list->addField('code', function($error) use($mode) {
+            $output = $error['code'];
+
+            if($mode != 'list') {
+                $output = $this->http->statusCodeToString($output);
+            }
+
+            return $output;
+        });
+    }
+
+    public function describeRequestField($list, $mode) {
+        $list->addField('request', function($error) use($mode) {
+            if(!$error['request']) {
+                return;
+            }
+
+            $request = $this->directory->newRequest($error['request']);
+            $name = $request->path->toString();
+
+            return $this->html->link($error['request'], $name)
+                ->setTitle($name);
+        });
+    }
+
+    public function describeQueryField($list) {
+        $list->addField('queryData', function($error) {
+            if(!$error['request']) {
+                return;
+            }
+         
+            $query = $this->directory->newRequest($error['request'])->query;
+
+            if($query->isEmpty()) {
+                return;
+            }
+
+            return $this->_describeQueryNode($query);
+        });
+    }
+
+    protected function _describeQueryNode($node) {
+        $list = $this->html->attributeList($node);
+
+        foreach($node->getKeys() as $key) {
+            $list->addField($key, $key, function($node) use($key) {
+                $node = $node->{$key};
+                $output = [];
+
+                if($node->hasValue()) {
+                    $value = $node->getValue();
+
+                    if($key == 'rf' || $key == 'rt') {
+                        $value = arch\Request::decode($value);
+                    }
+
+                    $output[] = $value;
+                }
+
+                if(count($node)) {
+                    $output[] = $this->_describeQueryNode($node);
+                }
+
+                return $output;
+            });
+        }
+
+        return $list;
+    }
+
+    public function describeMessageField($list, $mode) {
+        $list->addField('message', function($error) use($mode) {
+            $output = $error['message'];
+
+            if($mode == 'list') {
+                $output = $this->format->shorten($output, 40);
+            }
+
+            return $output;
+        });
+    }
+
+    public function describeUserField($list) {
+        $list->addField('user', function($error) {
+            return $this->import->component('UserLink', '~admin/users/clients/', $error['user'])
+                ->isNullable(true)
+                ->setDisposition('transitive');
+        });
+    }
+
+    public function describeIsProductionField($list) {
+        $list->addField('isProduction', $this->_('Prod.'), function($error) {
+            return $this->html->booleanIcon($error['isProduction']);
+        });
+    }
+
+    public function describeUserAgentField($list) {
+        $list->addField('userAgent', function($error) {
+            return $this->html->element('code', $error['userAgent']);
+        });
+    }
+}
--- directory/admin/system/accessErrors/_templates/Index.html.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Menu
-echo $this->import->component('IndexHeaderBar', '~admin/system/access-errors/');
-
-// List
-echo $this->import->component('ErrorList', '~admin/system/access-errors/');
