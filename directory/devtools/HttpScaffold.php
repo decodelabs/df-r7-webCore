e44f22b45e72495b1e4f2539732737c0b48d11aa
Tom Wright <tom@inflatablecookie.com>
2014-09-29 13:35:24 +0100

    Converted base devtools action to scaffold
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/devtools/HttpScaffold.php
+++ directory/devtools/HttpScaffold.php
@@ -1,18 +1,23 @@
-<?php 
+<?php
 /**
  * This file is part of the Decode Framework
  * @license http://opensource.org/licenses/MIT
  */
-namespace df\apex\directory\devtools\_menus;
+namespace df\apex\directory\devtools;
 
 use df;
 use df\core;
 use df\apex;
 use df\arch;
-    
-class Index_WebCore extends arch\navigation\menu\Base {
 
-    protected function _createEntries(arch\navigation\IEntryList $entryList) {
+class HttpScaffold extends arch\scaffold\template\AreaMenu {
+    
+    const DEFAULT_ACCESS = arch\IAccess::DEV;
+    const DIRECTORY_TITLE = 'Devtools';
+    const DIRECTORY_ICON = 'debug';
+    const HEADER_BAR = false;
+
+    public function generateIndexMenu($entryList) {
         $entryList->addEntries(
             $entryList->newLink('~devtools/users/', 'User setup')
                 ->setId('users')
@@ -26,35 +31,29 @@ class Index_WebCore extends arch\navigation\menu\Base {
                 ->setIcon('database')
                 ->setWeight(20),
 
-            $entryList->newLink('~devtools/mail/', 'Mail utilities')
-                ->setId('mail')
-                ->setDescription('View development mail and test active mail transports')
-                ->setIcon('mail')
-                ->setWeight(30),
-
             $entryList->newLink('~devtools/tasks/', 'Task manager')
                 ->setId('taskManager')
                 ->setDescription('Queue and schedule tasks and view logs of previously run processes')
                 ->setIcon('task')
-                ->setWeight(40),
+                ->setWeight(30),
 
             $entryList->newLink('~devtools/theme/', 'Theme configuration')
                 ->setId('theme')
                 ->setDescription('Change settings and define layouts for available site themes')
                 ->setIcon('theme')
-                ->setWeight(50),
+                ->setWeight(40),
 
             $entryList->newLink('~devtools/application/', 'Application utilities')
                 ->setId('application')
                 ->setDescription('View stats, generate testing and production versions etc.')
                 ->setIcon('stats')
-                ->setWeight(60),
+                ->setWeight(50),
 
             $entryList->newLink('~devtools/cache/', 'Cache control')
                 ->setId('cache')
                 ->setDescription('Refresh, clear and view stats for most cache structures your site employs')
                 ->setIcon('toolkit')
-                ->setWeight(70)
+                ->setWeight(60)
         );
     }
 }
