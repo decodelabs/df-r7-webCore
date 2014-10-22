e4dfdca1296f9498a5ab23714ac6dc446020071b
Tom Wright <tom@inflatablecookie.com>
2014-10-22 16:58:08 +0100

    Added stub site model
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ models/site/Model.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\models\site;
+
+use df;
+use df\core;
+use df\apex;
+use df\axis;
+
+class Model extends axis\Model {
+    
+}
