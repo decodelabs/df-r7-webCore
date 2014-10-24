68f59031a48ae31880c6a09fb404bb9c31774a47
Tom Wright <tom@inflatablecookie.com>
2014-10-24 20:46:53 +0100

    Added log importer for pestControl
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/admin/system/pestControl/HttpScaffold.php
+++ directory/admin/system/pestControl/HttpScaffold.php
@@ -43,4 +43,88 @@ class HttpScaffold extends arch\scaffold\template\AreaMenu {
                 ->setWeight(30)
         );
     }
+
+
+    public function importAction() {
+        $access = $critical = $miss = 0;
+
+        foreach($this->data->log->accessError->select()->orderBy('date ASC') as $orig) {
+            $new = $this->data->pestControl->accessLog->newRecord([
+                    'date' => $orig['date'],
+                    'mode' => $orig['mode'],
+                    'code' => $orig['code'],
+                    'request' => $orig['request'],
+                    'message' => $orig['message'],
+                    'userAgent' => $this->data->user->agent->logAgent($orig['userAgent']),
+                    'userId' => (string)$orig['user'],
+                    'userName' => null,
+                    'isProduction' => $orig['isProduction']
+                ]);
+
+            $new->save();
+            $access++;
+        }
+
+        foreach($this->data->log->criticalError->select()->orderBy('date ASC') as $orig) {
+            $error = $this->data->pestControl->error->logError(
+                $orig['exceptionType'],
+                null,
+                $orig['file'],
+                $orig['line'],
+                $orig['message']
+            );
+
+            if($error['seen'] == 1) {
+                $error['firstSeen'] = clone $orig['date'];
+            }
+
+            $error['lastSeen'] = clone $orig['date'];
+            $error->save();
+
+            $new = $this->data->pestControl->errorLog->newRecord([
+                    'date' => $orig['date'],
+                    'error' => $error,
+                    'mode' => $orig['mode'],
+                    'request' => $orig['request'],
+                    'message' => null,
+                    'userAgent' => $this->data->user->agent->logAgent($orig['userAgent']),
+                    'stackTrace' => $this->data->pestControl->stackTrace->logJson($orig['stackTrace']),
+                    'userId' => (string)$orig['user'],
+                    'userName' => null,
+                    'isProduction' => $orig['isProduction']
+                ]);
+
+            $new->save();
+            $critical++;
+        }
+
+        foreach($this->data->log->notFound->select()->orderBy('date ASC') as $orig) {
+            $miss = $this->data->pestControl->miss->logMiss(
+                $orig['request'],
+                $orig['mode']
+            );
+
+            if($miss['seen'] == 1) {
+                $miss['firstSeen'] = clone $orig['date'];
+            }
+
+            $miss['lastSeen'] = clone $orig['date'];
+            $miss->save();
+
+            $new = $this->data->pestControl->missLog->newRecord([
+                    'date' => $orig['date'],
+                    'miss' => $miss,
+                    'referrer' => $orig['referrer'],
+                    'message' => $orig['message'],
+                    'userId' => (string)$orig['user'],
+                    'userName' => null,
+                    'isProduction' => $orig['isProduction']
+                ]);
+
+            $new->save();
+            $miss++;
+        }
+
+        core\dump('Imported '.$access.' access logs, '.$critical.' error logs and '.$miss.' 404 logs');
+    }
 }
