19d2d514013b889308c82891d553c3c7819378c7
Tom Wright <tom@inflatablecookie.com>
2014-12-03 17:01:52 +0000

    Added automatic touch icon support
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ directory/front/HttpRootActionRouter.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\directory\front;
+
+use df;
+use df\core;
+use df\apex;
+use df\arch;
+
+class HttpRootActionRouter extends core\application\http\RootActionRouter {
+    
+    protected $_matches = [
+        '/^(apple-)?touch-icon(.+)?\.png/' => 'appleTouchIcon'
+    ];
+
+    public function appleTouchIcon(arch\IRequest $request, $matches) {
+        $request->path->setFilename('application-image');
+
+        if(isset($matches[2])) {
+            $parts = explode('-', trim($matches[2], '-'));
+
+            foreach($parts as $part) {
+                if($part == 'precomposed') {
+                    $request->query->precomposed = true;
+                } else if(preg_match('/^([0-9]+)x([0-9]+)$/', $part, $sizes)) {
+                    $request->query->width = $sizes[1];
+                    $request->query->height = $sizes[2];
+                }
+            }
+        } else {
+            $request->query->width = 180;
+            $request->query->height = 180;
+        }
+        
+        return $request;
+    }
+}
---
+++ directory/front/_actions/HttpApplicationImage.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\directory\front\_actions;
+
+use df;
+use df\core;
+use df\apex;
+use df\arch;
+use df\neon;
+
+class HttpApplicationImage extends arch\Action {
+    
+    public function executeAsPng() {
+        $theme = $this->aura->getTheme();
+
+        if(!$path = $theme->getApplicationImagePath()) {
+            $this->throwError(404, 'No application image path set');
+        }
+
+        $absPath = $theme->findAsset($path);
+        $type = core\io\Type::fileToMime($absPath);
+
+        if(!$absPath) {
+            $this->throwError(404, 'Application image '.$path.' not found');
+        }
+
+        if(isset($this->request->query->width)) {
+            $width = $this->request->query['width'];
+            $height = $this->request->query->get('height', $width);
+            $cache = neon\raster\Cache::getInstance();
+            $absPath = $cache->getTransformationFilePath($absPath, '[rs:'.$width.'|'.$height.']');
+        }
+
+        return $this->http->fileResponse($absPath)
+            ->setContentType($type);
+    }
+}
