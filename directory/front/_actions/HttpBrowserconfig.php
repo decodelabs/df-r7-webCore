884729087ac31d22abf6a62b7d26dd9bb4b0cc5e
Tom Wright <tom@inflatablecookie.com>
2014-12-03 17:51:40 +0000

    Added browserconfig action
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ directory/front/_actions/HttpBrowserconfig.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\directory\front\_actions;
+
+use df;
+use df\core;
+use df\apex;
+use df\arch;
+use df\neon;
+
+class HttpBrowserconfig extends arch\Action {
+    
+    const DEFAULT_ACCESS = arch\IAccess::ALL;
+    const CHECK_ACCESS = false;
+
+    public function executeAsXml() {
+        $view = $this->aura->getView('Browserconfig.xml');
+        $theme = $this->aura->getTheme();
+
+        $view['hasImage'] = (bool)$theme->getApplicationImagePath();
+        $view['tileColor'] = $theme->getApplicationColor()->toHexString();
+
+        return $view;
+    }
+}
---
+++ directory/front/_templates/Browserconfig.xml.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<browserconfig>
+   <msapplication>
+     <tile>
+        <?php if($this['hasImage']) { ?>
+        <square70x70logo src="<?php echo $this->uri('touch-icon-70x70.png?cts'); ?>"/>
+        <square150x150logo src="<?php echo $this->uri('touch-icon-150x150.png?cts'); ?>"/>
+        <square310x310logo src="<?php echo $this->uri('touch-icon-310x310.png?cts'); ?>"/>
+        <?php } ?>
+        <TileColor><?php echo $this->esc($this['tileColor']); ?></TileColor>
+     </tile>
+   </msapplication>
+</browserconfig>
