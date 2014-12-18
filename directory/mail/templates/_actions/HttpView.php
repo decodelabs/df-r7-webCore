5400608f1bbe9892447b5143d0c32dff9ed69d92
Tom Wright <tom@inflatablecookie.com>
2014-12-18 14:36:22 +0000

    Updated mail template viewer
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/mail/templates/_actions/HttpView.php
+++ directory/mail/templates/_actions/HttpView.php
@@ -23,6 +23,15 @@ class HttpView extends arch\Action {
         }
 
         $notification = $mail->renderPreview()->toNotification();
-        return $this->http->stringResponse($notification->getBodyHtml(), 'text/html');
+        $html = $notification->getBodyHtml();
+
+        $container = $this->aura->getWidgetContainer();
+        $view = $container->getView();
+        $view->setTheme(false);
+        $container->push($view->html->string($html));
+        $view->shouldUseLayout(false);
+        $view->setTitle($notification->getSubject());
+
+        return $view;
     }
 }
