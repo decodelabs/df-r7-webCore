7f1b3c24efc1df79527ac9d29930e710bf547519
Tom Wright <tom@inflatablecookie.com>
2015-10-07 21:37:10 +0100

    Extended theme dependency handler
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- themes/whitewash/Theme.php
+++ themes/whitewash/Theme.php
@@ -11,15 +11,15 @@ use df\apex;
 use df\aura;
 
 class Theme extends aura\theme\Base {
-    
+
     protected $_dependencies = [
-        'jquery-2.1' => 'jquery#~2.1'
+        'jquery#~2.1'
     ];
 
     public function applyDefaultIncludes(aura\view\IView $view) {
         $view
             ->linkCss('theme://sass/style.scss')
-            ->linkJs('asset://vendor/jquery-2.1/dist/jquery.min.js')
+            ->linkJs('asset://vendor/jquery/2.1/dist/jquery.min.js')
             ->linkJs('theme://js/main.js')
             ->linkFavicon('theme://favicon.ico')
             ;
