5536560e90df75c3641023f1bc92e7189f0f88ef
Tom Wright <tom@inflatablecookie.com>
2015-10-08 15:27:43 +0100

    Converted dependency resource links to dep resolver
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- themes/whitewash/Theme.php
+++ themes/whitewash/Theme.php
@@ -19,7 +19,7 @@ class Theme extends aura\theme\Base {
     public function applyDefaultIncludes(aura\view\IView $view) {
         $view
             ->linkCss('theme://sass/style.scss')
-            ->linkJs('asset://vendor/jquery/2.1/dist/jquery.min.js')
+            ->linkJs('dependency://jquery')
             ->linkJs('theme://js/main.js')
             ->linkFavicon('theme://favicon.ico')
             ;
