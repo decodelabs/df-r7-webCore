1a32e375019f131c7f2afcc235713228ad346725
Tom Wright <tom@inflatablecookie.com>
2015-12-24 16:49:39 +0000

    Updated ui view list node
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/ui/_nodes/HttpIndex.php
+++ directory/ui/_nodes/HttpIndex.php
@@ -19,11 +19,25 @@ class HttpIndex extends arch\node\Base {
             $this->throwError(401, 'Dev mode only');
         }
 
-        $view = $this->apex->view('__Index.html');
-        $files = df\Launchpad::$loader->lookupFileListRecursive('apex/directory/ui/_templates', 'php');
-        unset($files['__Index.html.php']);
-        $view['files'] = $files;
+        return $this->apex->newWidgetView(function($view) {
+            $files = df\Launchpad::$loader->lookupFileListRecursive('apex/directory/ui/_templates', 'php');
 
-        return $view;
+            yield $this->html->collectionList($files)
+                ->addField('name', function($filePath, $context) {
+                    $name = substr($context->getKey(), 0, -4);
+                    return $this->html->link(
+                            '~ui/view?path='.$name,
+                            $name
+                        )
+                        ->setIcon('file')
+                        ->setDisposition('informative');
+                })
+                ->addField('created', function($filePath) {
+                    return $this->format->date(@filectime($filePath));
+                })
+                ->addField('modified', function($filePath) {
+                    return $this->format->timeFromNow(@filemtime($filePath));
+                });
+        });
     }
 }
