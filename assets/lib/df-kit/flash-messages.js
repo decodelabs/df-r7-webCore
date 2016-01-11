e0eb78a84492799bcd065b41e79a30f978baa3ef
Tom Wright <tom@inflatablecookie.com>
2016-01-11 16:20:54 +0000

    Updated flashList handling
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- assets/lib/df-kit/flash-messages.js
+++ assets/lib/df-kit/flash-messages.js
@@ -6,9 +6,16 @@ define([
         init: function() {
             $('.w-flashList .w-flashMessage').each(function() {
                 $(this).prepend('<a class="close"><span data-icon="&#xe00b;" aria-hidden="true"></span></a>');
-            }).find('a.close').click(function() {
+            }).find('a.close').click(function(e) {
+                e.preventDefault();
+
                 $(this).parent().addClass('out').fadeOut(200, function() {
+                    var $parent = $(this).parent();
                     $(this).remove();
+
+                    if(!$parent.find('.w-flashMessage').length) {
+                        $parent.remove();
+                    }
                 });
             });
         }
