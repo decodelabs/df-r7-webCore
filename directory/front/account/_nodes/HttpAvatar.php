57e9aef5c95a174db9e81d6a189d80eba864214d
Tom Wright <tom@inflatablecookie.com>
2016-01-13 19:52:37 +0000

    Updated default avatar form ui
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/front/account/_nodes/HttpAvatar.php
+++ directory/front/account/_nodes/HttpAvatar.php
@@ -42,46 +42,38 @@ class HttpAvatar extends arch\node\Form {
             $versions = null;
         }
 
+        $form = $this->content->addForm()->addClass('stacked');
+        $form->setEncoding($form::ENC_MULTIPART);
+        $fs = $form->addFieldSet($this->_('Update your avatar'));
+
         if(!empty($versions)) {
-            $form = $this->content->addForm();
-            $fs = $form->addFieldSet($this->_('Select existing avatar'));
             $fa = $fs->addField($this->_('Your images'));
             $activeId = $this->_file['#activeVersion'];
 
             foreach($versions as $version) {
-                $fa->push($this->html('div', [
+                $fa->push($this->html('div.w-card', [
                         $this->html->tag('input', [
-                                'type' => 'image',
-                                'src' => $this->uri($version->getImageUrl('[cz:100|100]')),
-                                'name' => 'formEvent',
-                                'value' => $this->eventName('selectVersion', $version['id'])
-                            ])
-                            ->setStyle('display', 'block'),
+                            'type' => 'image',
+                            'src' => $this->uri($version->getImageUrl('[cz:150|150]')),
+                            'name' => 'formEvent',
+                            'value' => $this->eventName('selectVersion', $version['id'])
+                        ]),
 
                         $this->html->eventButton($this->eventName('deleteVersion', $version['id']), $this->_('Delete'))
                             ->setIcon('delete')
                     ])
                     ->setStyle('display', 'inline-block')
-                    ->setStyle('border', '4px solid #FFF')
-                    ->setStyle('padding', '0.5em')
                     ->chainIf($activeId == $version['id'], function($widget) {
-                        $widget->addClass('status-active')->setStyle('border', '4px #FEFFBA solid');
+                        $widget->addClass('active');
                     })
                 );
            }
         }
 
-
-        $form = $this->content->addForm();
-        $form->setEncoding($form::ENC_MULTIPART);
-
-        // Upload
-        $form->addFieldSet($this->_('Upload new avatar'))->push(
-            $this->getDelegate('upload')
-        );
+        $fs->push($this->getDelegate('upload'));
 
         // Buttons
-        $form->addButtonArea(
+        $fs->addButtonArea(
             $this->html->eventButton('upload', $this->_('Upload'))
                 ->setIcon('upload')
                 ->setDisposition('positive'),
