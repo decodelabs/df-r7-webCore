2d24a55e0dbd997a69e1b28be7715ac222f2ba21
Tom Wright <tom@inflatablecookie.com>
2016-01-21 17:50:24 +0000

    Added form-event input helper and error scroller to df-kit core
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- assets/lib/df-kit/core.js
+++ assets/lib/df-kit/core.js
@@ -21,6 +21,36 @@ define([
             var $body = $(document.body);
             this.location = $body.data('location');
             this.layout = $body.data('layout');
+
+
+
+            // Form event
+            $('input[data-formevent]:enabled').bind('keypress.formEvent', function(e) {
+                if(e.keyCode == '13') {
+                    var s = $('#form-hidden-activeFormEvent');
+                    var f = $(this).parents('form');
+                    var event = $(this).attr('data-formevent');
+
+                    if(!s.length && event != 'default') {
+                        f.prepend('<input type="hidden" id="form-hidden-activeFormEvent" name="formEvent" />');
+                        s = $('#form-hidden-activeFormEvent');
+                    }
+
+                    if(s.length) {
+                        s.val(event);
+                    }
+
+                    e.preventDefault();
+                    f.submit();
+                }
+            });
+
+            // Scroll to first error
+            if($('.w-fieldError').length) {
+                $('html, body').animate({
+                    scrollTop: $('.w-fieldError').first().parent().offset().top
+                }, 200);
+            }
         },
 
         call: function(callback, data) {
