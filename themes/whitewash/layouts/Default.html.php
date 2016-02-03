db96fdcd68bad02ca48c85f6fd331284ac530037
Tom Wright <tom@inflatablecookie.com>
2016-02-03 14:56:05 +0000

    Added user switch box to whitewash layout
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- themes/whitewash/layouts/Default.html.php
+++ themes/whitewash/layouts/Default.html.php
@@ -1,23 +1,42 @@
 <div class="layout-pageArea">
     <header class="layout-header">
         <h1><?php echo $this->html->link('/', $this->application->getName()); ?></h1>
-        
-        
-        <?php 
+
+        <?php
+        echo $this->html('nav.user', function() {
+            if($this->user->isLoggedIn()) {
+                yield $this->_('Logged in as %n%. ', [
+                    '%n%' => $this->user->client->getFullName()
+                ]);
+
+                yield $this->html->link('account/logout', 'Log out')
+                    ->setIcon('user')
+                    ->setDisposition('transitive');
+            } else {
+                yield $this->_('Browsing as a guest ');
+                yield $this->html->link(
+                        $this->uri('account/login', true),
+                        $this->_('Log in now')
+                    )
+                    ->setIcon('user')
+                    ->setDisposition('transitive');
+            }
+        });
+
         if($this->context->request->hasPath()) {
             echo $this->html->breadcrumbList(true);
-        } 
+        }
         ?>
     </header>
 
     <?php echo $this->html->flashList(); ?>
-    
+
     <div class="layout-contentArea">
         <?php echo $this->renderInnerContent(); ?>
     </div>
 
     <footer>
-    <?php 
+    <?php
     echo $this->html->menuBar()
         ->addLinks(
             $this->html->link('~admin/', $this->_('Admin control panel'))
