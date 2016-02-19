8325525064baae8749d708ea87a23557417fc507
Tom Wright <tom@inflatablecookie.com>
2016-02-19 11:54:25 +0000

    Updated email verify email template
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/mail/account/_templates/EmailVerify.html.php
+++ directory/mail/account/_templates/EmailVerify.html.php
@@ -6,7 +6,7 @@ if($user->isNew()) { ?>
 <?php } else { ?>
 <h4>Your email address has recently changed on <?php echo $this->esc($this->application->getName()); ?>.</h4>
 <?php } ?>
-<p>To make sure your details are correct and we can contact you, please click the link below to verify this email address..</p>
+<p>To make sure your details are correct and we can contact you, please click the link below to verify this email address.</p>
 <?php
 echo $this->html('p',
     $this->html->link('account/email-verify?key='.$key.'&user='.$user['id'], $this->_(
