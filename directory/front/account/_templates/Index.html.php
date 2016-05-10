66eab25f2643ea1160535e9c482226c985323197
Tom Wright <tom@inflatablecookie.com>
2016-05-10 16:34:13 +0100

    Updated default account index page
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/front/account/_templates/Index.html.php
+++ directory/front/account/_templates/Index.html.php
@@ -1,5 +1,5 @@
-<?php 
-$client = $this->context->user->client; 
+<?php
+$client = $this->context->user->client;
 
 echo $this->html('p', [
     $this->html->_('Hello %n%, you last logged in %t% ago', [
@@ -8,4 +8,7 @@ echo $this->html('p', [
     ])
 ]);
 
-echo $this->html->link('account/logout', 'Logout')->setIcon('arrow-left');
+echo $this->html('p', $this->html->link('account/avatar', 'Update avatar')->setIcon('image'));
+echo $this->html('p', $this->html->link('account/change-password', 'Change password')->setIcon('edit'));
+echo $this->html('p', $this->html->link('account/logout', 'Logout')->setIcon('arrow-left'));
+echo $this->html('p', $this->html->link('account/deactivate', 'Close my account')->setIcon('remove'));
