a29a1ce7766002ea02a8293d418b2acc910e8b4a
Tom Wright <tom@inflatablecookie.com>
2016-09-13 17:40:01 +0100

    Added customUploader delegates
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ directory/shared/media/_formDelegates/CustomTempUploader.php
@@ -0,0 +1,290 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\directory\shared\media\_formDelegates;
+
+use df;
+use df\core;
+use df\apex;
+use df\arch;
+use df\aura;
+use df\link;
+
+class CustomTempUploader extends arch\node\form\Delegate implements
+    arch\node\ISelectionProviderDelegate,
+    core\io\IAcceptTypeProcessor {
+
+    use arch\node\TForm_SelectorDelegate;
+    use core\io\TAcceptTypeProcessor;
+
+    private $_dirChecked = false;
+    private $_hasUploaded = false;
+
+    protected function _getTempDir() {
+        $destination = $this->getStore('tempUploadDir');
+
+        if(!$this->_dirChecked) {
+            $dir = core\fs\Dir::createUploadTemp($destination);
+            $this->_dirChecked = true;
+            $this->setStore('tempUploadDir', $dir->getPath());
+
+            return $dir;
+        }
+
+        return core\fs\Dir::factory($destination);
+    }
+
+    protected function _getFileList() {
+        static $files;
+
+        if(!isset($files)) {
+            $tempDir = $this->_getTempDir();
+            $files = [];
+            $i = 0;
+
+            foreach($tempDir->scanFiles() as $fileName => $file) {
+                $time = $file->getLastModified();
+
+                $files[$time.$i] = [
+                    'fileName' => $fileName,
+                    'size' => $file->getSize(),
+                    'time' => $time
+                ];
+
+                $i++;
+            }
+
+            krsort($files);
+
+            if(!$this->_isForMany) {
+                while(count($files) > 1) {
+                    $tempDir->deleteFile(array_pop($files)['fileName']);
+                }
+            }
+        }
+
+        return $files;
+    }
+
+    public function render($callback=null) {
+        if(!$callback) {
+            $callback = [$this, '_render'];
+        }
+
+        if($form = $this->content->findFirstWidgetOfType('Form')) {
+            $form->setEncoding($form::ENC_MULTIPART);
+        }
+
+        $tempDir = $this->_getTempDir();
+        $files = $this->_getFileList();
+
+        if(!$this->_isForMany) {
+            $available = array_shift($files);
+        } else {
+            $available = $files;
+        }
+
+        return core\lang\Callback($callback, $this, $available);
+    }
+
+    public function _render($delegate, $available) {
+        yield $this->html('span', null, ['id' => $this->getWidgetId()]);
+
+        if(!$this->_isForMany) {
+            if($available) {
+                yield $this->html('div.w-selected', function() use($available) {
+                    yield $this->html->hidden($this->fieldName('selectUpload'), $available['fileName']);
+
+                    yield [
+                        $this->html('span.fileName', $available['fileName']), ' ',
+                        $this->html->number($this->format->fileSize($available['size']))
+                    ];
+
+                    yield $this->html->eventButton(
+                            $this->eventName('removeFile', $available['fileName']),
+                            $this->_('Remove')
+                        )
+                        ->setDisposition('negative')
+                        ->setIcon('cross')
+                        ->shouldValidate(false);
+                });
+            }
+        } else {
+            yield $this->html->bulletList($available, function($file) {
+                yield $this->html->checkbox(
+                    $this->fieldName('selectUpload['.$file['fileName'].']'),
+                    $this->values->selectUpload->{$file['fileName']},
+                    [
+                        $this->html('span.fileName', $file['fileName']), ' ',
+                        $this->html->number($this->format->fileSize($file['size']))
+                    ]
+                );
+
+                yield $this->html->eventButton(
+                        $this->eventName('removeFile', $file['fileName']),
+                        $this->_('Remove')
+                    )
+                    ->setDisposition('negative')
+                    ->setIcon('cross')
+                    ->shouldValidate(false)
+                    ->addClass('remove');
+            })->addClass('w-selected');
+        }
+
+        yield $this->html('div.w-inputArea', [
+            $input = $this->html->fileUpload($this->fieldName('file'), $this->values->file)
+                ->setAcceptTypes(...$this->getAcceptTypes())
+                ->setId($this->getWidgetId().'-input'),
+
+            $this->html->label($this->_('Choose a file...'), $input)
+                ->addClass('btn hidden')
+                ->addClass(!empty($available) ? 'replace': null)
+        ]);
+
+        yield $this->html->eventButton(
+                $this->eventName('upload'),
+                $this->_('Upload')
+            )
+            ->setIcon('upload')
+            ->setDisposition('positive')
+            ->shouldValidate(false)
+            ->addClass('upload');
+    }
+
+    public function getWidgetId() {
+        return $this->format->slug('ctu-'.$this->getDelegateId());
+    }
+
+
+// Result
+    protected function onUploadEvent() {
+        if($this->_hasUploaded) {
+            return;
+        }
+
+        unset($this->values->file);
+
+        $uploadHandler = new link\http\upload\Handler();
+        $uploadHandler->setAcceptTypes(...$this->_acceptTypes);
+
+        if(!count($uploadHandler)) {
+            return $this->http->redirect('#'.$this->getWidgetId());
+        }
+
+        if(!$this->_isForMany) {
+            unset($this->values->selectUpload);
+        }
+
+        $tempDir = $this->_getTempDir();
+
+        if($file = $uploadHandler[$this->fieldName('file')]) {
+            $file->upload($tempDir, $this->values->file);
+
+            if($file->isSuccess()) {
+                if(!$this->_isForMany) {
+                    $this->values->selectUpload = $file->getBasename();
+                } else {
+                    $this->values->selectUpload[$file->getBasename()] = true;
+                }
+            }
+        }
+
+        $this->_hasUploaded = true;
+        return $this->http->redirect('#'.$this->getWidgetId());
+    }
+
+    public function hasAnyFile() {
+        return (bool)count($this->_getFileList());
+    }
+
+    public function onRemoveFileEvent($fileName) {
+        $tempDir = $this->_getTempDir();
+        $tempDir->deleteFile($fileName);
+        return $this->http->redirect('#'.$this->getWidgetId());
+    }
+
+    public function apply() {
+        $this->onUploadEvent();
+
+        if(!$this->values->file->isValid()) {
+            return null;
+        }
+
+        if(!$this->_isForMany) {
+            $fileName = $this->values['selectUpload'];
+
+            if(!strlen($fileName)) {
+                if($this->_isRequired) {
+                    $this->values->file->addError('required', $this->_(
+                        'You must upload a file'
+                    ));
+                }
+
+                return null;
+            }
+
+            $tempDir = $this->_getTempDir();
+
+            if(!$tempDir->hasFile($fileName)) {
+                $this->logs->logException(new \Exception('Couldn\'t find temp upload file: '.$tempDir->getPath().'/'.$fileName));
+
+                $this->values->file->addError('notFound', $this->_(
+                    'Something went wrong while transferring your file - please try again'
+                ));
+
+                return null;
+            }
+
+            return $tempDir.'/'.$fileName;
+        } else {
+            $fileNames = $this->values->selectUpload->getKeys();
+
+            if(empty($fileNames)) {
+                if($this->_isRequired) {
+                    $this->values->file->addError('required', $this->_(
+                        'You must upload at least one file'
+                    ));
+                }
+
+                return [];
+            }
+
+            $tempDir = $this->_getTempDir();
+            $output = [];
+
+            foreach($fileNames as $fileName) {
+                if(!$tempDir->hasFile($fileName)) {
+                    $this->logs->logException(new \Exception('Couldn\'t find temp upload file: '.$tempDir->getPath().'/'.$fileName));
+
+                    $this->values->file->addError('notFound', $this->_(
+                        'Something went wrong while transferring your file - please try again'
+                    ));
+
+                    unset($this->values->selectUpload->{$fileName});
+                    return [];
+                }
+
+                $output[] = $tempDir.'/'.$fileName;
+            }
+
+            return $output;
+        }
+    }
+
+    public function handlePostEvent(arch\node\IActiveForm $target, string $event, array $args) {
+        $required = $this->_isRequired;
+        $this->_isRequired = false;
+        $this->handleEvent('upload');
+        $this->_isRequired = $required;
+    }
+
+    protected function onComplete($success) {
+        if($destination = $this->getStore('tempUploadDir')) {
+            core\fs\Dir::delete($destination);
+        }
+
+        core\fs\Dir::purgeUploadTemp();
+    }
+}
---
+++ directory/shared/media/_formDelegates/CustomUploader.php
@@ -0,0 +1,332 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\directory\shared\media\_formDelegates;
+
+use df;
+use df\core;
+use df\apex;
+use df\arch;
+use df\aura;
+use df\mesh;
+use df\flex;
+
+class CustomUploader extends arch\node\form\Delegate implements
+    arch\node\IDependentDelegate,
+    arch\node\ISelectorDelegate,
+    core\io\IAcceptTypeProcessor {
+
+    use arch\node\TForm_SelectorDelegate;
+    use arch\node\TForm_ValueListSelectorDelegate;
+    use arch\node\TForm_DependentDelegate;
+    use arch\node\TForm_MediaBucketAwareSelector;
+    use core\io\TAcceptTypeProcessor;
+
+    protected $_limit = null;
+    protected $_ownerId;
+
+    public function setOwnerId($id) {
+        $this->_ownerId = $id;
+        return $this;
+    }
+
+    public function getOwnerId() {
+        if($this->_ownerId !== null) {
+            return $this->_ownerId;
+        } else {
+            return $this->user->client->getId();
+        }
+    }
+
+    public function setFileLimit($limit) {
+        if($limit) {
+            $limit = (int)$limit;
+        } else {
+            $limit = null;
+        }
+
+        $this->_limit = $limit;
+        return $this;
+    }
+
+    public function getFileLimit() {
+        return $this->_limit;
+    }
+
+    public function getSourceEntityLocator() {
+        return new mesh\entity\Locator('upload://File');
+    }
+
+    protected function init() {
+        $this->_setupBucket();
+    }
+
+    protected function loadDelegates() {
+        if($this->_bucketHandler) {
+            $accept = array_merge($this->_bucketHandler->getAcceptTypes(), $this->_acceptTypes);
+        } else {
+            $accept = $this->_acceptTypes;
+        }
+
+        $this->loadDelegate('upload', 'CustomTempUploader')
+            ->isRequired($this->_isRequired)
+            ->isForMany($this->_isForMany)
+            ->setAcceptTypes(...$accept);
+    }
+
+
+// Render
+    public function render($callback=null) {
+        if(!$this->_bucket) {
+            $this->throwError(500, 'No bucket has been set');
+        }
+
+        //return $this['upload']->render();
+
+        return $this['upload']->render(function($delegate, $available) use($callback) {
+            if($this->_isForMany || empty($available)) {
+                $query = $this->data->media->file->select('id as fileId', 'creationDate as time')
+                    ->joinRelation('activeVersion', 'fileName', 'fileSize as size')
+                    ->where('file.id', 'in', (array)$this->getSelected())
+                    ->orderBy('creationDate ASC');
+
+                if(!$this->_isForMany) {
+                    $available = $query->toRow();
+                } else {
+                    $files = $query->toArray();
+                    $count = count($available);
+
+                    foreach($files as $i => $file) {
+                        $salt = $count + $i;
+                        $ts = $file['time']->toTimestamp();
+                        $key = $ts.$salt;
+                        $file['time'] = $ts;
+                        $available[$key] = $file;
+                    }
+
+                    ksort($available);
+
+                    foreach($available as &$file) {
+                        if(!isset($file['fileId'])) {
+                            $file['fileId'] = null;
+                        }
+                    }
+                    unset($file);
+                }
+
+            }
+
+            if(!$callback) {
+                $callback = [$this, '_render'];
+            }
+
+            return core\lang\Callback($callback, $this, $available);
+        });
+    }
+
+    public function _render($delegate, $available) {
+        $delegate = $this['upload'];
+
+        yield $this->html('span', null, ['id' => $delegate->getWidgetId()]);
+
+        if($this instanceof arch\node\IDependentDelegate) {
+            $messages = $this->getDependencyMessages();
+
+            if(!empty($messages)) {
+                foreach($messages as $key => $value) {
+                    yield $this->html->flashMessage($value, 'warning');
+                }
+                return;
+            }
+        }
+
+        if($messages = $this->_getSelectionErrors()) {
+            yield $this->html->fieldError($messages);
+        }
+
+        if(!$this->_isForMany) {
+            if($available) {
+                yield $this->html('div.w-selected', function() use($delegate, $available) {
+                    if(isset($available['fileId'])) {
+                        yield $this->html->hidden($this->fieldName('selected'), $available['fileId']);
+
+                        yield [
+                            $this->html('span.fileName', $available['fileName']), ' ',
+                            $this->html->number($this->format->fileSize($available['size']))
+                        ];
+
+                        yield $this->html->booleanIcon(true);
+
+                        yield $this->html->eventButton(
+                                $this->eventName('removeFile', $available['fileId']),
+                                $this->_('Remove')
+                            )
+                            ->setDisposition('negative')
+                            ->setIcon('cross')
+                            ->shouldValidate(false)
+                            ->addClass('remove');
+                    } else {
+                        yield $this->html->hidden($delegate->fieldName('selectUpload'), $available['fileName']);
+
+                        yield [
+                            $this->html('span.fileName', $available['fileName']), ' ',
+                            $this->html->number($this->format->fileSize($available['size']))
+                        ];
+
+                        yield $this->html->eventButton(
+                                $delegate->eventName('removeFile', $available['fileName']),
+                                $this->_('Remove')
+                            )
+                            ->setDisposition('negative')
+                            ->setIcon('cross')
+                            ->shouldValidate(false)
+                            ->addClass('remove');
+                    }
+                });
+            }
+        } else {
+            yield $this->html->bulletList($available, function($file) use($delegate) {
+                if(isset($file['fileId'])) {
+                    yield $this->html->checkbox(
+                        $this->fieldName('selected[]'),
+                        $this->values->selected->contains($file['fileId']),
+                        [
+                            $this->html('span.fileName', $file['fileName']), ' ',
+                            $this->html->number($this->format->fileSize($file['size']))
+                        ],
+                        $file['fileId']
+                    );
+
+                    yield $this->html->booleanIcon(true);
+
+                    yield $this->html->eventButton(
+                            $this->eventName('removeFile', $file['fileId']),
+                            $this->_('Remove')
+                        )
+                        ->setDisposition('negative')
+                        ->setIcon('cross')
+                        ->shouldValidate(false);
+                } else {
+                    yield $this->html->checkbox(
+                        $delegate->fieldName('selectUpload['.$file['fileName'].']'),
+                        $delegate->values->selectUpload->{$file['fileName']},
+                        [
+                            $this->html('span.fileName', $file['fileName']), ' ',
+                            $this->html->number($this->format->fileSize($file['size']))
+                        ]
+                    );
+
+                    yield $this->html->eventButton(
+                            $delegate->eventName('removeFile', $file['fileName']),
+                            $this->_('Remove')
+                        )
+                        ->setDisposition('negative')
+                        ->setIcon('cross')
+                        ->shouldValidate(false)
+                        ->addClass('remove');
+                }
+            })->addClass('w-selected');
+        }
+
+        yield $this->html('div.w-inputArea', [
+            $input = $this->html->fileUpload($delegate->fieldName('file'), $delegate->values->file)
+                ->setAcceptTypes(...$delegate->getAcceptTypes())
+                ->setId($delegate->getWidgetId().'-input'),
+
+            $this->html->label($this->_('Choose a file...'), $input)
+                ->addClass('btn hidden')
+                ->addClass(!empty($available) ? 'replace': null)
+        ]);
+
+        yield $this->html->eventButton(
+                $delegate->eventName('upload'),
+                $this->_('Upload')
+            )
+            ->setIcon('upload')
+            ->setDisposition('positive')
+            ->shouldValidate(false)
+            ->addClass('upload');
+    }
+
+    public function hasAnyFile() {
+        if($this->hasSelection()) {
+            return true;
+        }
+
+        return $this['upload']->hasAnyFile();
+    }
+
+    public function apply() {
+        if(!$this->_bucket) {
+            return;
+        }
+
+        $delegate = $this['upload'];
+        $delegate->isRequired($this->_isRequired && !$this->hasSelection());
+        $delegate->values->file->clearErrors();
+
+        if(!$this->_isForMany) {
+            if($filePath = $delegate->apply()) {
+                $file = $this->data->media->publishFile($filePath, $this->_bucket, [
+                    'owner' => $this->getOwnerId()
+                ]);
+                $this->setSelected($file['id']);
+            }
+        } else {
+            $filePaths = $delegate->apply();
+            $ids = $this->getSelected();
+
+            if(!empty($filePaths)) {
+                foreach($filePaths as $filePath) {
+                    $file = $this->data->media->publishFile($filePath, $this->_bucket, [
+                        'owner' => $this->getOwnerId()
+                    ]);
+
+                    $ids[] = $file['id'];
+                }
+            }
+
+            if($this->_limit) {
+                $ids = array_slice($ids, count($ids) - $this->_limit);
+            }
+
+            $this->setSelected($ids);
+        }
+
+
+        if($this->_isRequired && !$this->hasSelection()) {
+            if($this->_isForMany) {
+                $delegate->values->file->addError('required', $this->_(
+                    'You must upload at least one file'
+                ));
+            } else {
+                $delegate->values->file->addError('required', $this->_(
+                    'You must upload a file'
+                ));
+            }
+        }
+
+
+        if($this->isValid()) {
+            $delegate->setComplete(true);
+        }
+
+        return $this->getSelected();
+    }
+
+    public function onRemoveFileEvent($id) {
+        if($this->_isForMany) {
+            unset($this->values->selected->{$id});
+        } else {
+            if($this->values['selected'] == $id) {
+                unset($this->values->selected);
+            }
+        }
+    }
+
+    protected function _sanitizeSelection($selection) {
+        return (string)flex\Guid::factory($selection);
+    }
+}
