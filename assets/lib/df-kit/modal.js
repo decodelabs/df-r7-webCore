b8c70debfa08672db608aac8ca2649ad88599e6b
Tom Wright <tom@inflatablecookie.com>
2016-11-30 15:38:22 +0000

    Renamed modal wrapper
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- assets/lib/df-kit/modal.js
+++ assets/lib/df-kit/modal.js
@@ -6,15 +6,15 @@ define([
     return Core.component({
         attr: {
             trigger: '.modal, [data-modal]',
-            container: '#modal-overlay',
-            wrapper: '#modal-wrapper',
+            overlay: '#modal-overlay',
+            scroll: '#modal-scroll',
             content: '#modal-content'
         },
 
         _closeCallback: null,
         _overlayAction: 'close',
         _currentOptions: null,
-        _containerFadeTime: 200,
+        _overlayFadeTime: 200,
         _contentFadeTime: 0,
         client: null,
 
@@ -25,7 +25,7 @@ define([
                 e.preventDefault();
 
                 var modalClass = $(this).data('modal-class'),
-                    containerClass = $(this).data('modal-overlay-class'),
+                    overlayClass = $(this).data('modal-overlay-class'),
                     href = $(this).data('modal-href'),
                     overlayAction = $(this).data('overlay-action');
 
@@ -40,7 +40,7 @@ define([
 
                 _this.load(href, {
                     class: modalClass,
-                    containerClass: containerClass,
+                    overlayClass: overlayClass,
                     overlayAction: overlayAction
                 });
             });
@@ -98,16 +98,16 @@ define([
             options = options || {};
             Core.trigger('dialog.open', 'modal');
 
-            var $container = $(_this.attr.container),
-                $modal, $overlay,
+            var $overlay = $(_this.attr.overlay),
+                $modal, $combined,
                 builder = function() {
                     _this._currentOptions = options;
                     $modal = $(_this.attr.content).hide().html(html);
-                    $overlay = $(_this.attr.container + ',' + _this.attr.wrapper).removeClass('modal-close');
+                    $combined = $(_this.attr.overlay + ',' + _this.attr.scroll).removeClass('modal-close');
                     _this._closeCallback = options.closeCallback;
 
                     if(options.class) $modal.addClass(options.class);
-                    if(options.containerClass) $container.addClass(options.containerClass);
+                    if(options.overlayClass) $overlay.addClass(options.overlayClass);
 
                     Core.call(options.callback, options.callbackData);
                     $modal.fadeIn(_this._contentFadeTime);
@@ -118,29 +118,29 @@ define([
 
                         case 'close':
                         default:
-                            $overlay.addClass('modal-close');
+                            $combined.addClass('modal-close');
                             break;
                     }
                 };
 
-            if($container.length) {
+            if($overlay.length) {
                 $(_this.attr.content).fadeOut(200, function() {
                     Core.call(_this._closeCallback);
                     _this._closeCallback = null;
 
                     if(_this._currentOptions) {
                         if(_this._currentOptions.class) $(_this.attr.content).removeClass(_this._currentOptions.class);
-                        if(_this._currentOptions.containerClass) $container.removeClass(_this._currentOptions.containerClass);
+                        if(_this._currentOptions.overlayClass) $overlay.removeClass(_this._currentOptions.overlayClass);
                     }
 
                     builder();
                 });
             } else {
                 $('html').addClass('modal-open');
-                $container = $('<div id="modal-overlay"><div id="modal-wrapper"><div id="modal-content"></div></div></div>').hide().appendTo('body');
+                $overlay = $('<div id="modal-overlay"><div id="modal-scroll"><div id="modal-content"></div></div></div>').hide().appendTo('body');
                 $(_this.attr.content).hide();
 
-                $container.fadeIn(_this._containerFadeTime, function() {
+                $overlay.fadeIn(_this._overlayFadeTime, function() {
                     builder();
                 });
             }
@@ -150,8 +150,8 @@ define([
             var _this = this,
                 isComplete = _this.client && _this.client.lastResponse && _this.client.lastResponse.isComplete;
 
-            if(!isComplete && $('.w-form', this.attr.container).length) {
-                var $form = $('.w-form', this.attr.container).first();
+            if(!isComplete && $('.w-form', this.attr.overlay).length) {
+                var $form = $('.w-form', this.attr.overlay).first();
 
                 Ajax.post($form.attr('action'), {
                     data: [{name:'formEvent', value:'cancel'}],
@@ -177,15 +177,15 @@ define([
                 }
             };
 
-            $(_this.attr.container + ',' + _this.attr.wrapper).removeClass('modal-close');
+            $(_this.attr.overlay + ',' + _this.attr.scroll).removeClass('modal-close');
 
-            if(!$(_this.attr.container).length) {
+            if(!$(_this.attr.overlay).length) {
                 callbackRunner();
             } else {
                 $(_this.attr.content).fadeOut(_this._contentFadeTime, function() {
                     _this._overlayAction = 'close';
 
-                    $(_this.attr.container).fadeOut(_this._containerFadeTime, function() {
+                    $(_this.attr.overlay).fadeOut(_this._overlayFadeTime, function() {
                         $(this).remove();
                         $('html').removeClass('modal-open');
                         callbackRunner();
