efc1bab33278f0bedceba0c5f6e2e58a9f5c7940
Tom Wright <tom@inflatablecookie.com>
2018-10-08 15:18:20 +0100

    Added image thumbnail in media list
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/admin/media/files/HttpScaffold.php
+++ directory/admin/media/files/HttpScaffold.php
@@ -11,8 +11,8 @@ use df\apex;
 use df\arch;
 use df\opal;
 
-class HttpScaffold extends arch\scaffold\RecordAdmin {
-
+class HttpScaffold extends arch\scaffold\RecordAdmin
+{
     const TITLE = 'Files';
     const ICON = 'file';
     const ADAPTER = 'axis://media/File';
@@ -23,7 +23,7 @@ class HttpScaffold extends arch\scaffold\RecordAdmin {
     ];
 
     const LIST_FIELDS = [
-        'fileName', 'bucket', 'fileSize', 'owner',
+        'thumbnail', 'fileName', 'bucket', 'fileSize', 'owner',
         'creationDate', 'version'
     ];
 
@@ -32,11 +32,12 @@ class HttpScaffold extends arch\scaffold\RecordAdmin {
         'hash', 'creationDate'
     ];
 
-// Record data
-    protected function prepareRecordList($query, $mode) {
+    // Record data
+    protected function prepareRecordList($query, $mode)
+    {
         $query
             ->countRelation('versions')
-            ->leftJoinRelation('activeVersion', 'number as version', 'fileSize')
+            ->leftJoinRelation('activeVersion', 'number as version', 'fileSize', 'contentType')
             ->importRelationBlock('bucket', 'link')
             ->importRelationBlock('owner', 'link')
             ->paginate()
@@ -44,21 +45,23 @@ class HttpScaffold extends arch\scaffold\RecordAdmin {
                 ->end();
     }
 
-    public function deleteRecord(opal\record\IRecord $file, array $flags=[]) {
+    public function deleteRecord(opal\record\IRecord $file, array $flags=[])
+    {
         $this->data->media->deleteFile($file);
         return $this;
     }
 
 
-// Sections
-    public function renderDetailsSectionBody($file) {
+    // Sections
+    public function renderDetailsSectionBody($file)
+    {
         return $this->html->panelSet()
             ->addPanel(parent::renderDetailsSectionBody($file))
-            ->addPanel(function() use($file) {
-                if(false !== strpos($file['activeVersion']['contentType'], 'image/')) {
+            ->addPanel(function () use ($file) {
+                if (false !== strpos($file['activeVersion']['contentType'], 'image/')) {
                     $image = $this->html->image($file->getImageUrl('[rs:380|380|f]'));
 
-                    if($file['activeVersion']['contentType'] == 'image/svg+xml') {
+                    if ($file['activeVersion']['contentType'] == 'image/svg+xml') {
                         $image->setStyle('height', '25em');
                     }
 
@@ -67,7 +70,8 @@ class HttpScaffold extends arch\scaffold\RecordAdmin {
             });
     }
 
-    public function renderVersionsSectionBody($file) {
+    public function renderVersionsSectionBody($file)
+    {
         return $this->apex->scaffold('./versions/')
             ->renderRecordList(
                 $file->versions->select(),
@@ -76,12 +80,14 @@ class HttpScaffold extends arch\scaffold\RecordAdmin {
     }
 
 
-// Components
-    protected function getParentSectionRequest() {
+    // Components
+    protected function getParentSectionRequest()
+    {
         return '../files?bucket='.$this->getRecord()['#bucket'];
     }
 
-    public function getRecordOperativeLinks($record, $mode) {
+    public function getRecordOperativeLinks($record, $mode)
+    {
         return [
             $this->html->link($this->media->getDownloadUrl($record['id']), $this->_('Download file'))
                 ->setIcon('download')
@@ -92,22 +98,40 @@ class HttpScaffold extends arch\scaffold\RecordAdmin {
     }
 
 
-// Fields
-    public function defineBucketField($list, $mode) {
-        $list->addField('bucket', function($file) {
+    // Fields
+    public function defineThumbnailField($list, $mode)
+    {
+        $list->addField('thumbnail', $this->_('Thumb'), function ($file) {
+            if (false !== strpos($file['contentType'], 'image/')) {
+                $image = $this->media->image($file['id'], '[rs:80|80|f]');
+
+                if ($file['activeVersion']['contentType'] == 'image/svg+xml') {
+                    $image->setStyle('height', '6em');
+                }
+
+                return $image;
+            }
+        });
+    }
+
+    public function defineBucketField($list, $mode)
+    {
+        $list->addField('bucket', function ($file) {
             return $this->apex->component('../BucketLink', $file['bucket']);
         });
     }
 
-    public function defineUrlField($list, $mode) {
-        $list->addField('url', $this->_('Copy & paste url'), function($file) {
+    public function defineUrlField($list, $mode)
+    {
+        $list->addField('url', $this->_('Copy & paste url'), function ($file) {
             return $this->html('code', 'media/download/f'.$file['id']);
         });
     }
 
-    public function defineFileSizeField($list, $mode) {
-        $list->addField('fileSize', $this->_('Size'), function($file) use($mode) {
-            if($mode == 'list') {
+    public function defineFileSizeField($list, $mode)
+    {
+        $list->addField('fileSize', $this->_('Size'), function ($file) use ($mode) {
+            if ($mode == 'list') {
                 return $this->format->fileSize($file['fileSize']);
             } else {
                 return $this->format->fileSize($file['activeVersion']['fileSize']);
@@ -115,25 +139,28 @@ class HttpScaffold extends arch\scaffold\RecordAdmin {
         });
     }
 
-    public function defineHashField($list, $mode) {
-        $list->addField('hash', function($file) {
-            if($hash = $this->format->binHex($file['activeVersion']['hash'])) {
+    public function defineHashField($list, $mode)
+    {
+        $list->addField('hash', function ($file) {
+            if ($hash = $this->format->binHex($file['activeVersion']['hash'])) {
                 return $this->html('samp', $hash);
             }
         });
     }
 
-    public function defineOwnerField($list, $mode) {
-        $list->addField('owner', function($file) {
+    public function defineOwnerField($list, $mode)
+    {
+        $list->addField('owner', function ($file) {
             return $this->apex->component('~admin/users/clients/UserLink', $file['owner'])
                 ->isNullable(true);
         });
     }
 
-    public function defineVersionField($list, $mode) {
-        if($mode == 'list') {
-            $list->addField('version', $this->_('Ver.'), function($file) {
-                if($file['versions'] != $file['version']) {
+    public function defineVersionField($list, $mode)
+    {
+        if ($mode == 'list') {
+            $list->addField('version', $this->_('Ver.'), function ($file) {
+                if ($file['versions'] != $file['version']) {
                     return $this->html->_('<strong class="warning">%v%</strong> of <strong>%t%</strong>', [
                         '%v%' => $file['version'],
                         '%t%' => $file['versions']
