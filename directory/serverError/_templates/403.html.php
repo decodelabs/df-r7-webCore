bc11737e5cb1fe8d24326703b79262339c8995fa
Tom Wright <tom@inflatablecookie.com>
2019-04-05 21:06:41 +0100

    Removed server error templates
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/serverError/_templates/403.html.php
+++
@@ -1,8 +0,0 @@
-<?php
-$this->view->setTitle('Page not accessible');
-?>
-<header class="hero error e403">
-    <h1 class="logo">Acowtancy</h1>
-    <h2>Sorry, that's not for you!</h2>
-    <h3>...but maybe we can find something else to look at?</h3>
-</header>
--- directory/serverError/_templates/500.html.php
+++
@@ -1,8 +0,0 @@
-<?php
-$this->view->setTitle('Problems...');
-?>
-<header class="hero error e500">
-    <h1 class="logo">Acowtancy</h1>
-    <h2>Oh man, something went wrong</h2>
-    <h3>...you can bet we're trying to fix it though!</h3>
-</header>
