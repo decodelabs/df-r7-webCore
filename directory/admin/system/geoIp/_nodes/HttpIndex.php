45f67a6dfba2647f1b9a72b7b6972ee1744b53dc
Tom Wright <tom@inflatablecookie.com>
2019-05-15 16:55:17 +0100

    Updated geoIP detection in system admin
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/admin/system/geoIp/_nodes/HttpIndex.php
+++ directory/admin/system/geoIp/_nodes/HttpIndex.php
@@ -12,14 +12,35 @@ use df\arch;
 use df\opal;
 use df\link;
 
-class HttpIndex extends arch\node\Base {
-
-    public function executeAsHtml() {
+class HttpIndex extends arch\node\Base
+{
+    public function executeAsHtml()
+    {
         $view = $this->apex->view('Index.html');
         $handler = link\geoIp\Handler::factory();
 
+        $ip = $this->http->getIp();
+        $view['isLoopback'] = false;
+
+        if ($ip->isLoopback()) {
+            $view['isLoopback'] = true;
+            $ip = link\Ip::factory($_SERVER['SERVER_ADDR']);
+
+            if ($ip->isLoopback()) {
+                try {
+                    $client = new link\http\Client();
+                    $response = $client->get('http://ipecho.net/plain');
+
+                    if ($response->isOk()) {
+                        $ip = link\Ip::factory($response->getContent());
+                    }
+                } catch (\Throwable $e) {
+                }
+            }
+        }
+
         $view['config'] = link\geoIp\Config::getInstance();
-        $view['result'] = $handler->lookup($this->http->getIp());
+        $view['result'] = $handler->lookup($ip);
         $view['adapterList'] = link\geoIp\Handler::getAdapterList();
 
         return $view;
