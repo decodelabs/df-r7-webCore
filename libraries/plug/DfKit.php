8155c2191e129bd638a21b6e896c196d01a927c2
Tom Wright <tom@inflatablecookie.com>
2019-06-25 18:26:27 +0100

    Updated require.js invokation
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/plug/DfKit.php
+++ libraries/plug/DfKit.php
@@ -11,48 +11,52 @@ use df\plug;
 use df\arch;
 use df\aura;
 
-class DfKit implements arch\IDirectoryHelper {
-
+class DfKit implements arch\IDirectoryHelper
+{
     use arch\TDirectoryHelper;
     use aura\view\TView_DirectoryHelper;
 
     protected static $_isInit = false;
 
-    protected function _init() {
-        if(!$this->view) {
+    protected function _init()
+    {
+        if (!$this->view) {
             throw core\Error::{'aura/view/ENoView,ENoContext'}(
                 'View is not available in plugin context'
             );
         }
     }
 
-    public function init() {
-        if(self::$_isInit) {
+    public function init()
+    {
+        if (self::$_isInit) {
             return $this;
         }
 
         $url = '/df-kit/bootstrap.js?theme='.$this->view->getTheme()->getId();
 
-        if(df\Launchpad::$compileTimestamp) {
+        if (df\Launchpad::$compileTimestamp) {
             $url .= '&cts='.df\Launchpad::$compileTimestamp;
         } else {
             $url .= '&cts='.time();
         }
 
+        // Avoid using data-main for extensions also using RequireJS
         $this->view->linkJs('dependency://requirejs', 1, [
-            'data-main' => $this->view->uri($url)
+            '__invoke' => 'require([\''.$this->view->uri($url).'\']);'
         ]);
 
         return $this;
     }
 
-    public function load(...$modules) {
+    public function load(...$modules)
+    {
         $this->init();
 
         $current = $this->view->bodyTag->getDataAttribute('require');
         $modules = core\collection\Util::flatten($modules);
 
-        if(!empty($current)) {
+        if (!empty($current)) {
             $modules = array_unique(array_merge(explode(' ', $current), $modules));
         }
 
