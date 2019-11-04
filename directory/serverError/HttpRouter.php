7cc91e963b346087673fe14018b2e3abe87c58c6
Tom Wright <tom@inflatablecookie.com>
2019-11-04 18:12:59 +0000

    Updated serverError router
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/serverError/HttpRouter.php
+++ directory/serverError/HttpRouter.php
@@ -15,14 +15,16 @@ class HttpRouter extends arch\router\Base
 {
     public function routeIn(arch\IRequest $request)
     {
-        $type = $node = $query = null;
-        $comp = $request->getComponents();
-        extract($comp);
+        $type = $request->getType();
+        $node = $request->getRawNode();
+        $query = $request->getQuery();
 
         $output = $this->newRequest('~serverError/index.'.strtolower((string)$type));
+        
         $output->query->import([
             'error' => $node
         ]);
+
         $output->importQuery($query);
 
         return $output;
@@ -30,9 +32,7 @@ class HttpRouter extends arch\router\Base
 
     public function routeOut(arch\IRequest $request)
     {
-        $query = null;
-        $comp = $request->getComponents();
-        extract($comp);
+        $query = $request->getQuery();
 
         return $this->newRequest('~serverError/'.$query['error'])
             ->setFragment($request->getFragment())
