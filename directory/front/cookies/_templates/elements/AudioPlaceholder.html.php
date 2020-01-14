9d34f8ca0013516fec324c0a7bee59dca88abe38
Tom Wright <tom@inflatablecookie.com>
2020-01-14 14:14:55 +0000

    Updated cookie placeholders
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/front/cookies/_templates/elements/AudioPlaceholder.html.php
+++ directory/front/cookies/_templates/elements/AudioPlaceholder.html.php
@@ -9,3 +9,24 @@ $category = Html::iList($category, null, ', ', ' and ');
         <?php echo $this->html->link('cookies/settings', 'Cookie Settings')->addClass('modal')->setIcon('settings'); ?>.</p>
     </div>
 </div>
+
+
+<?php
+
+echo Html::{'div.audio-cookie-placeholder > div.audio-message'}(function () {
+    $category = (array)($this['categories'] ?? 'statistics');
+    $category = Html::iList($category, null, ', ', ' and ');
+
+    yield Html::{'p.strong'}(['This audio is hosted on a service that uses ', $category, 'tracking cookies.']);
+    yield Html::{'p'}([
+        'These cookies are currently disabled - to listen to this audio, you will need to consent to and re-enable ',
+        $category, ' cookies in your ',
+        $this->html->link('cookies/settings', 'Cookie Settings')
+            ->addClass('modal')
+            ->setIcon('settings')
+    ]);
+
+    yield $this->html->link('cookies/dismiss-notice', 'Enable all cookies')
+        ->addClass('btn')
+        ->setIcon('tick');
+});
--- directory/front/cookies/_templates/elements/VideoPlaceholder.html.php
+++ directory/front/cookies/_templates/elements/VideoPlaceholder.html.php
@@ -1,11 +1,19 @@
 <?php
-$category = (array)($this['categories'] ?? 'statistics');
-$category = Html::iList($category, null, ', ', ' and ');
-?>
-<div class="video-cookie-placeholder">
-    <div class="video-message">
-        <p class="strong">This video is hosted on a service that uses <?php echo $category; ?> tracking cookies.</p>
-        <p>These cookies are currently disabled - to view this video, you will need to consent to and re-enable <?php echo $category; ?> cookies in your
-        <?php echo $this->html->link('cookies/settings', 'Cookie Settings')->addClass('modal')->setIcon('settings'); ?>.</p>
-    </div>
-</div>
+
+echo Html::{'div.video-cookie-placeholder > div.video-message'}(function () {
+    $category = (array)($this['categories'] ?? 'statistics');
+    $category = Html::iList($category, null, ', ', ' and ');
+
+    yield Html::{'p.strong'}(['This video is hosted on a service that uses ', $category, 'tracking cookies.']);
+    yield Html::{'p'}([
+        'These cookies are currently disabled - to view this video, you will need to consent to and re-enable ',
+        $category, ' cookies in your ',
+        $this->html->link('cookies/settings', 'Cookie Settings')
+            ->addClass('modal')
+            ->setIcon('settings')
+    ]);
+
+    yield $this->html->link('cookies/dismiss-notice', 'Enable all cookies')
+        ->addClass('btn')
+        ->setIcon('tick');
+});
