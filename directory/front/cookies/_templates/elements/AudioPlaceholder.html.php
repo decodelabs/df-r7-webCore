d3a02d78cce67d25dc715a2cb88c7746cf050500
Tom Wright <tom@inflatablecookie.com>
2020-01-14 14:21:35 +0000

    Updated cookie audio placeholder
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/front/cookies/_templates/elements/AudioPlaceholder.html.php
+++ directory/front/cookies/_templates/elements/AudioPlaceholder.html.php
@@ -1,17 +1,4 @@
 <?php
-$category = (array)($this['categories'] ?? 'statistics');
-$category = Html::iList($category, null, ', ', ' and ');
-?>
-<div class="audio-cookie-placeholder">
-    <div class="audio-message">
-        <p class="strong">This audio is hosted on a service that uses <?php echo $category; ?> tracking cookies.</p>
-        <p>These cookies are currently disabled - to listen to this audio, you will need to consent to and re-enable <?php echo $category; ?> cookies in your
-        <?php echo $this->html->link('cookies/settings', 'Cookie Settings')->addClass('modal')->setIcon('settings'); ?>.</p>
-    </div>
-</div>
-
-
-<?php
 
 echo Html::{'div.audio-cookie-placeholder > div.audio-message'}(function () {
     $category = (array)($this['categories'] ?? 'statistics');
