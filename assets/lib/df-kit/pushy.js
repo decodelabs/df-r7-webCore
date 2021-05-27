e875753f6a40912322063883ddb91a596dcc9b1e
Tom Wright <tom@inflatablecookie.com>
2021-05-27 14:53:57 +0000

    Removed touchstart from pushy
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- assets/lib/df-kit/pushy.js
+++ assets/lib/df-kit/pushy.js
@@ -2,7 +2,7 @@ define([
     'jquery',
     'df-kit/core',
     'df-kit/ajax'
-], function($, Core, Ajax) {
+], function ($, Core, Ajax) {
     return Core.component({
         attr: {
             trigger: '.pushy, [data-pushy]',
@@ -14,24 +14,24 @@ define([
         _contentFadeTime: 200,
         client: null,
 
-        init: function() {
+        init: function () {
             var _this = this;
 
-            $(document).on('click', this.attr.trigger, function(e) {
+            $(document).on('click', this.attr.trigger, function (e) {
                 e.preventDefault();
 
                 var pushyClass = $(this).data('pushy-class'),
                     href = $(this).data('pushy-href'),
                     side = $(this).data('pushy-side')
-                    closeButton = $(this).data('pushy-close-button');
+                closeButton = $(this).data('pushy-close-button');
 
-                if(!href) {
+                if (!href) {
                     href = $(this).data('pushy');
-                    if(href === true) href = null;
+                    if (href === true) href = null;
                 }
 
-                if(!href) href = $(this).attr('href');
-                if(!side) side = 'right';
+                if (!href) href = $(this).attr('href');
+                if (!side) side = 'right';
 
                 _this.load(href, {
                     class: pushyClass,
@@ -40,33 +40,33 @@ define([
                 });
             });
 
-            $(document).on('click touchstart', '.pushy-close', function(e) {
-                if(e.target != e.currentTarget) return;
+            $(document).on('click', '.pushy-close', function (e) {
+                if (e.target != e.currentTarget) return;
                 e.preventDefault();
                 _this.close();
             });
 
-            $(document).keyup(function(e) {
-                if(e.which == 27) _this.close();
+            $(document).keyup(function (e) {
+                if (e.which == 27) _this.close();
             });
 
-            Core.on('dialog.open', function(source) {
-                if(source !== 'pushy') {
+            Core.on('dialog.open', function (source) {
+                if (source !== 'pushy') {
                     //_this.close();
                 }
             });
         },
 
 
-        _importOptions: function(options, response) {
+        _importOptions: function (options, response) {
             var keys = {
                 class: ['dialogClass', 'pushyClass'],
                 closeButton: ['dialogCloseButton', 'pushyCloseButton']
             };
 
-            $.each(keys, function(option, importKeys) {
-                $.each(importKeys, function(i, importKey) {
-                    if(typeof response[importKey] !== typeof undefined) {
+            $.each(keys, function (option, importKeys) {
+                $.each(importKeys, function (i, importKey) {
+                    if (typeof response[importKey] !== typeof undefined) {
                         options[option] = response[importKey];
                     }
                 });
@@ -75,28 +75,28 @@ define([
             return options;
         },
 
-        load: function(href, options) {
+        load: function (href, options) {
             var _this = this,
                 deferred = $.Deferred();
 
-            _this.open('loading...', options).done(function($content) {
+            _this.open('loading...', options).done(function ($content) {
                 Ajax.embedInto($content, href, {
                     source: 'pushy',
                     live: options.live !== false
-                }).progress(function(client) {
+                }).progress(function (client) {
                     _this.client = client;
 
-                    _this.client.once('content:show', function(response) {
+                    _this.client.once('content:show', function (response) {
                         Core.trigger('dialog.load', 'pushy');
                     });
 
-                    _this.client.once('form:completeInitial', function(response) {
+                    _this.client.once('form:completeInitial', function (response) {
                         response.handled = true;
                         _this.close();
                     });
 
                     deferred.notify(client);
-                }).done(function(response, request) {
+                }).done(function (response, request) {
                     _this._importOptions(options, response);
                     _this._applyOptions(options);
                     deferred.resolve(response, request);
@@ -106,7 +106,7 @@ define([
             return deferred.promise();
         },
 
-        open: function(html, options) {
+        open: function (html, options) {
             var _this = this,
                 $doc = $('html'),
                 $leftContainer = $(_this.attr.leftContainer),
@@ -118,11 +118,11 @@ define([
 
 
             // Prepare
-            if($leftContainer.length) {
+            if ($leftContainer.length) {
                 _this.trigger('clear');
                 var $active = $(_this.attr.content, '.pushy-container.pushy-active');
 
-                if(!($active.length && !$doc.hasClass('push-'+options.side))) {
+                if (!($active.length && !$doc.hasClass('push-' + options.side))) {
                     $(_this.attr.content).hide();
                 }
             } else {
@@ -134,13 +134,13 @@ define([
             // Set active
             $('.pushy-container').removeClass('pushy-active');
 
-            if(!$doc.hasClass('push-'+options.side)) {
-                $doc.removeClass('push-left push-right').addClass('push-'+options.side);
+            if (!$doc.hasClass('push-' + options.side)) {
+                $doc.removeClass('push-left push-right').addClass('push-' + options.side);
             }
 
 
             // Load content
-            if(options.side == 'left') {
+            if (options.side == 'left') {
                 $content = $leftContainer.addClass('pushy-active').find('.pushy-content');
             } else {
                 $content = $rightContainer.addClass('pushy-active').find('.pushy-content');
@@ -150,7 +150,7 @@ define([
             $content.html(html);
 
             // Start anim
-            setTimeout(function() {
+            setTimeout(function () {
                 $doc.addClass('pushy-active');
             }, 1);
 
@@ -162,37 +162,37 @@ define([
             return deferred.promise();
         },
 
-        _applyOptions: function(options) {
+        _applyOptions: function (options) {
             var $container;
 
-            if(options.side == 'left') {
+            if (options.side == 'left') {
                 $container = $('.pushy-container.push-left');
             } else {
                 $container = $('.pushy-container.push-right');
             }
 
-            if(options.class) $container.attr('class', 'pushy-container push-'+options.side+' '+options.class);
+            if (options.class) $container.attr('class', 'pushy-container push-' + options.side + ' ' + options.class);
             $container.find('> a.pushy-close').toggleClass('hidden', options.closeButton === false);
         },
 
-        close: function() {
+        close: function () {
             var _this = this,
                 deferred = $.Deferred();
 
-            if(this.client) {
+            if (this.client) {
                 this.client.close('modal').done(deferred.notify);
             } else {
                 deferred.notify();
             }
 
-            deferred.progress(function() {
-                if(!$(_this.attr.content).length) {
+            deferred.progress(function () {
+                if (!$(_this.attr.content).length) {
                     deferred.resolve();
                 } else {
                     var $body = $('html');
                     $body.removeClass('pushy-active');
 
-                    setTimeout(function() {
+                    setTimeout(function () {
                         $body.removeClass('push-left push-right');
                         $('.pushy-container').remove();
                         deferred.resolve();
@@ -200,8 +200,8 @@ define([
                 }
             });
 
-            deferred.done(function() {
-                if(_this.client) _this.client.destroy();
+            deferred.done(function () {
+                if (_this.client) _this.client.destroy();
                 _this.trigger('clear');
                 _this.trigger('close');
             });
