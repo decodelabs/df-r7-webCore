57c646de2a2bcd65a8e187a9f53b28b7eb518f64
Tom Wright <tom@inflatablecookie.com>
2022-10-19 21:47:47 +0000

    Updated fire categories
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ libraries/fire/Category/Comment.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+
+namespace df\fire\Category;
+
+class Comment extends Base
+{
+    public const DEFAULT_BLOCKS = ['SimpleTags', 'Markdown', 'BBCode'];
+}
